195f5882ca0f4e31e05c82d4e38e0e51db9ede63
YassinNouh21 <yassinnouh21@gmail.com>
2024-11-27 19:02:21 +0200

    feat: Montenegrin Cupertino localizations for better support in 'cnr' locale

--- lib/montenegrin_localization.dart
+++ lib/montenegrin_localization.dart
@@ -1,5 +1,6 @@
 /// A Flutter localization package specifically designed for Montenegrin script (crnogorski/латиница) that provides comprehensive text translations and region-specific formatting.
 library;
 
+export 'src/montenegrin_cupertino_localizations.dart';
 export 'src/montenegrin_material_localizations.dart';
 export 'src/montenegrin_widgets_localizations.dart';
---
+++ lib/src/montenegrin_cupertino_localizations.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/date_symbol_data_custom.dart'
+as date_symbol_data_custom;
+import 'package:intl/date_symbols.dart' as intl;
+import 'package:intl/intl.dart' as intl;
+import 'package:montenegrin_localization/montenegrin_localization.dart';
+
+
+/// LocalizationDelegate for Montenegrin Cupertino Localizations.
+class _MontenegrinCupertinoLocalizationsDelegate
+    extends LocalizationsDelegate<CupertinoLocalizations> {
+  const _MontenegrinCupertinoLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => locale.languageCode == 'cnr';
+
+  @override
+  Future<CupertinoLocalizations> load(Locale locale) async {
+    final String localeName =
+    intl.Intl.canonicalizedLocale(locale.toString());
+
+    // Initialize custom date formatting for Montenegrin Latin.
+    date_symbol_data_custom.initializeDateFormattingCustom(
+      locale: localeName,
+      patterns: montenegrinLocaleDatePatterns,
+      symbols:
+      intl.DateSymbols.deserializeFromMap(montenegrinDateSymbols),
+    );
+
+    return SynchronousFuture<CupertinoLocalizations>(
+      MontenegrinCupertinoLocalizations(
+        localeName: localeName,
+        decimalFormat: intl.NumberFormat('#,##0.###', 'en_US'),
+        fullYearFormat: intl.DateFormat('y', localeName),
+        dayFormat: intl.DateFormat('yMd', localeName),
+        doubleDigitMinuteFormat: intl.DateFormat('yMMMd', localeName),
+        mediumDateFormat: intl.DateFormat('EEE, MMM d', localeName),
+        singleDigitHourFormat:
+        intl.DateFormat('EEEE, MMMM d, y', localeName),
+        singleDigitMinuteFormat:
+        intl.DateFormat('MMMM y', localeName),
+        singleDigitSecondFormat: intl.DateFormat('MMM d', localeName),
+      ),
+    );
+  }
+
+  @override
+  bool shouldReload(_MontenegrinCupertinoLocalizationsDelegate old) =>
+      false;
+}
+
+/// A custom set of localizations for the 'cnr' locale.
+/// Provides Montenegrin translations for Cupertino widgets.
+class MontenegrinCupertinoLocalizations
+    extends GlobalCupertinoLocalizations {
+  const MontenegrinCupertinoLocalizations({
+    super.localeName = 'cnr',
+    required super.fullYearFormat,
+    required super.dayFormat,
+    required super.doubleDigitMinuteFormat,
+    required super.singleDigitHourFormat,
+    required super.singleDigitMinuteFormat,
+    required super.singleDigitSecondFormat,
+    required super.decimalFormat,
+    required super.mediumDateFormat,
+  });
+
+  // #docregion Getters
+
+  @override
+  String get alertDialogLabel => 'Upozorenje';
+
+  @override
+  String get anteMeridiemAbbreviation => 'prijepodne';
+
+  @override
+  String get copyButtonLabel => 'Kopiraj';
+
+  @override
+  String get cutButtonLabel => 'Iseci';
+
+  @override
+  String get modalBarrierDismissLabel => 'Odbaci';
+
+  @override
+  String get pasteButtonLabel => 'Zalijepi';
+
+  @override
+  String get postMeridiemAbbreviation => 'popodne';
+
+  @override
+  String get selectAllButtonLabel => 'Odaberi sve';
+
+  @override
+  String get todayLabel => 'Danas';
+
+  @override
+  String get noSpellCheckReplacementsLabel => 'Nema prijedloga';
+
+  // #enddocregion Getters
+
+  // Implement other required getters with Montenegrin translations as needed.
+
+  static const LocalizationsDelegate<CupertinoLocalizations>
+  delegate = _MontenegrinCupertinoLocalizationsDelegate();
+
+  @override
+  String get datePickerDateOrderString => 'dMy';
+
+  @override
+  String get datePickerDateTimeOrderString => 'dMy Hms';
+
+  @override
+  String? get datePickerHourSemanticsLabelOther => 'sat';
+
+  @override
+  String? get datePickerMinuteSemanticsLabelOther => 'minuta';
+
+  @override
+  String get searchTextFieldPlaceholderLabel => 'Pretraga';
+
+  @override
+  String get tabSemanticsLabelRaw => 'Kartica od';
+
+  @override
+  String? get timerPickerHourLabelOther => 'sat';
+
+  @override
+  String? get timerPickerMinuteLabelOther => 'minuta';
+
+  @override
+  String? get timerPickerSecondLabelOther => 'sekunda';
+
+  @override
+  String get lookUpButtonLabel => 'Potraži';
+
+  @override
+  String get menuDismissLabel => 'Odbaci meni';
+
+  @override
+  String get searchWebButtonLabel => 'Pretraži web';
+
+  @override
+  String get shareButtonLabel => 'Podijeli...';
+
+  @override
+  String get clearButtonLabel => 'Očisti';
+}
